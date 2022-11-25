3c90d20da0cdae7fcdb7cb8cfa4ffc9dfecb9dd4
Andres Rios <ariosarbelaez@gmail.com>
2022-11-25 19:29:14 -0300

    Inicio de escritura de archivos de salida, modificacion gitignore

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ list_links = get_list.get_links()
 for link in list_links:
     URL = URL_root + link
     descripcion = des_prod.des_prod(URL)
+    descripcion = str(descripcion)
+    with open('outfile/descripcion.csv', 'w', encoding='utf-8') as f:
+        f.write(descripcion)
+
     funcion = func_empresa.func_emp(URL, link)
-    print(funcion)
+    funcion = str(funcion)
+    with open('outfile/funcion.csv', 'w', encoding='utf-8') as f:
+        f.write(funcion)
